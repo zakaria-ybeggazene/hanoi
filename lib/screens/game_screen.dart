2f681bc94800f61936981c8a778cb5b06c9dcc82
Zakaria Ybeggazene <iz_ybeggazene@esi.dz>
2019-10-10 16:52:07 +0100

    ExternalData

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/rod.dart';
+import '../widgets/rod_build.dart';
+import '../models/rod.dart';
+import '../models/disk.dart';
 
 class GameScreen extends StatefulWidget {
+  static List<Disk> list1 = [];
+  static List<Disk> list2 = [];
+  static List<Disk> list3 = [];
+  static Rod rod1 = Rod(id: 1, disksList: list1);
+  static Rod rod2 = Rod(id: 2, disksList: list2);
+  static Rod rod3 = Rod(id: 3, disksList: list3);
+
+  static List<Rod> rodList = [rod1, rod2, rod3];
+
   @override
   _GameScreenState createState() => _GameScreenState();
 }
@@ -22,18 +33,9 @@ class _GameScreenState extends State<GameScreen> {
           Container(
             child: Row(
               children: <Widget>[
-                Rod(
-                  initial: true,
-                  id: 1,
-                ),
-                Rod(
-                  initial: false,
-                  id: 2,
-                ),
-                Rod(
-                  initial: false,
-                  id: 3,
-                ),
+                RodBuild(GameScreen.rod1),
+                RodBuild(GameScreen.rod2),
+                RodBuild(GameScreen.rod3)
               ],
             ),
           ),
